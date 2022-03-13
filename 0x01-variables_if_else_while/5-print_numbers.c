895aa973731d5806b1a65ac79f61147f272cf84d
aokoowiro <aokoowiro@gmail.com>
2022-03-13 15:12:40 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all single decimal numbers starting from 0
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+int f;
+
+do {
+printf("%d", f);
+x++;
+} while (f <= 9);
+putchar('\n');
+return (0);
+}
