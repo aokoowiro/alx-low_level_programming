cfa76e1ab28494a3b1b690dd0457120d1ed7dc5d
aokoowiro <aokoowiro@gmail.com>
2022-03-13 16:12:14 +0000

    Print Comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Ptint all possible different combo of 3 digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int e, f, g, h;
+
+for (e = 0; e < 1000; i++)
+{
+f = e / 100; /* hundreds */
+g = (e / 10) % 10; /* tens */
+h = e % 10; /* unit */
+
+if (f < g && g < h)
+{
+putchar(f + '0');
+putchar(g + '0');
+putchar(h + '0');
+
+if (e < 700)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
