2eb8f2478f3d187d40bd5bd03b2fde2448ee1e01
aokoowiro <aokoowiro@gmail.com>
2022-03-16 23:09:52 +0000

    10 add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,15 +3,19 @@
 /**
  * add - adds two integers
  * Return: returns the added value
- * @a -parameter 1
- * @b -parameter 2
+ * @a -integer 1
+ * @b -integer 2
  */
 
-int add(int a, int b)
+int add(int, int)
+
+int = a;
+int = b;
+
 {
 
 int sum;
-
 sum = a + b;
 return (sum);
+
 }
