dcb1eecc1c1cfa7a0f4c686ac30923f340b51f5a
aokoowiro <aokoowiro@gmail.com>
2022-03-16 03:51:17 +0000

    8-24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,15 +13,15 @@ int mins;
 
 for (hrs = 0; hrs < 24; hrs++)
 {
-for (mins = 0; min < 60; mins++)
+for (mins = 0; mins < 60; mins++)
 {
-_putchar((hr / 10) + '0');
-_putchar((hr % 10) + '0');
+_putchar((hrs / 10) + '0');
+_putchar((hrs % 10) + '0');
 _putchar(':');
 _putchar((mins / 10) + '0');
 _putchar((mins % 10) + '0');
 _putchar('\n');
 }
 }
-return (0)
+return (0);
 }
