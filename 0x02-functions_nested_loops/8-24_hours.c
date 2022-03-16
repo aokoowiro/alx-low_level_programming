fc34df803f31a39a1565791e6bce6bd8d2b4ac44
aokoowiro <aokoowiro@gmail.com>
2022-03-16 03:42:19 +0000

    8-24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minutes of the day of Jack Bauer,
- * starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0.
  */
 
-void jack_bauer(void)
+jack_bauer(void)
 {
-  int fc, sc, tc, lc;
+int hrs;
+int mins;
 
-  fc = 0;
-  sc = 0;
-  tc = 0;
-  lc = 0;
-  while (1)
-    {
-      _putchar(fc + '0');
-      _putchar(sc + '0');
-      _putchar(':');
-      _putchar(tc + '0');
-      _putchar(lc + '0');
-      _putchar('\n');
-      lc++;
-      if (lc == 10)
-	{
-	  tc++;
-	  if (tc == 6)
+for (hr = 0; hr < 24; hr++)
 {
-sc++;
-if (sc == 4)
+for (min = 0; min < 60; min++)
 {
-fc++;
-if (fc == 3)
-{
-fc = 0;
-}
-sc = 0;
-}
-tc = 0;
-}
-lc = 0;
-}
-if (fc == 2 && sc == 3 && tc == 5 && lc == 9)
-{
-brea;
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
 }
 }
 }
