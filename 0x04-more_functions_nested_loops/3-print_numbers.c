4672b32d618252339b059b628df7385db701597f
Daisy-Beverly <aokoowiro@gmail.com>
2022-03-18 13:58:51 +0000

    task 3-10

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * print_most_numbers -prints numbers between zero,
- * nine except two and four
- * Return: void
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void print_most_numbers(void)
+int main(void)
 {
-int ch;
-
-for (ch = 48; ch < 58; ch++)
-{
-if (ch != 50 && ch != 52)
-_putchar(ch);
-}
-_putchar('\n');
+print_numbers();
+return (0);
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 - 14 ten times followed by a new line
+ * @void: takes no arguments
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int i, j;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i >= 10)
+_putchar(i / 10 + 48);
+_putchar(i % 10 + 48);
+}
+_putchar('\n');
+}
+}
