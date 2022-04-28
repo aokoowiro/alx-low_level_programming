027350760d3c749017f46e2409ce948931d91268
Daisy-Beverly <aokoowiro@gmail.com>
2022-04-28 22:36:48 +0000

    bit manipulation

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _checknum(char *argv[], int n)
+int main(int argc, char *argv[])
 
 #endif
--- 0x12-singly_linked_lists/.#4-free_list.c
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.1904:1650568482
